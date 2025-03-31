4a68bfdb0f32ef1726b1d1ad576d04ed211fca3f
Kota-704 <kktt14590358@gmail.com>
2025-03-31 23:21:01 +0900

    ✨ モデル設計

--- routine_project/reminder/models.py
+++ routine_project/reminder/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
-# Create your models here.
+class routineModel(models.Model):
+ id=models.AutoField(primary_key=True, verbose_name='ID')
+ title=models.CharField(max_length=100, verbose_name='タイトル')
+ period=models.IntegerField(null=True, blank=True, validators=[MinValueValidator(1)], help_text='例：7 = 毎週、30 = 毎月', verbose_name='周期')
+ create_at=models.TimeField(auto_now_add=True, verbose_name='作成日時')
+ url=models.URLField(null=True, blank=True, verbose_name='URL')
+ notice_text=models.TextField(max_length=400, verbose_name='通知内容')
