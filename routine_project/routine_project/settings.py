a83105d325d1d41e866f4d397ddd4a215ff49ca7
Kota-704 <kktt14590358@gmail.com>
2025-03-31 23:20:44 +0900

    ♻️ DBをpostgreに

--- routine_project/routine_project/settings.py
+++ routine_project/routine_project/settings.py
@@ -75,10 +75,15 @@ WSGI_APPLICATION = 'routine_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'routine_db',
+        'USER': 'postgres',
+        'PASSWORD': '1d#!Asq#o3s8',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
+
 
 
 # Password validation
