dd5db2a966d8a6832a76ee9e1817f200f3e5ec94
Kota-704 <kktt14590358@gmail.com>
2025-04-06 20:53:17 +0900

    ✨ サインアップ機能追加

--- routine_project/routine_project/settings.py
+++ routine_project/routine_project/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
 import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -44,6 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'reminder',
     'theme',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -80,16 +84,31 @@ WSGI_APPLICATION = 'routine_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST'),
-        'PORT': os.environ.get('DB_PORT', 5432),
+IS_RENDER = os.environ.get("RENDER", "") == "true"
+
+if IS_RENDER:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.environ.get('DB_NAME'),
+            'USER': os.environ.get('DB_USER'),
+            'PASSWORD': os.environ.get('DB_PASSWORD'),
+            'HOST': os.environ.get('DB_HOST'),
+            'PORT': os.environ.get('DB_PORT', 5432),
+        }
     }
-}
+else:
+    # ローカル用設定
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'routine_db',
+            'USER': 'postgres',
+            'PASSWORD': '1d#!Asq#o3s8',
+            'HOST': 'localhost',
+            'PORT': '5432'
+        }
+    }
 
 
 
--- routine_project/routine_project/urls.py
+++ routine_project/routine_project/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('reminder/', include("reminder.urls")),
     path('login/', auth_views.LoginView.as_view(), name="login"),
     path('logout/', auth_views.LogoutView.as_view(), name="logout"),
+    path('accounts/', include('accounts.urls')),
 ]
